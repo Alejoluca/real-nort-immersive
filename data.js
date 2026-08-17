126a0ec127a5f7426e090e0622e6c097df28a083
Alejoluca <139021138+Alejoluca@users.noreply.github.com>
2026-08-16 22:43:31 -0500

    Loader for ALL images: 4 parts, 46 props, 291 real Drive photos

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 var featured=[],allProperties=[];
 (function(){
   function go(){
-    var pool=[].concat(window.__RN_PART1||[],window.__RN_PART2||[],window.__RN_PART3||[]);
+    var pool=[].concat(window.__RN_PART1||[],window.__RN_PART2||[],window.__RN_PART3||[],window.__RN_PART4||[]);
     if(!pool.length){if(typeof window.__RN_ON_DATA==="function")window.__RN_ON_DATA();return}
     function shuffle(a){for(var i=a.length-1;i>0;i--){var j=Math.floor(Math.random()*(i+1));var t=a[i];a[i]=a[j];a[j]=t}return a}
     shuffle(pool);
@@ -12,13 +12,14 @@ var featured=[],allProperties=[];
   }
   function loadScript(src,cb){
     var s=document.createElement("script");
-    s.src=src+"?v=21";s.async=true;s.onload=cb;s.onerror=cb;
+    s.src=src+"?v=22";s.async=true;s.onload=cb;s.onerror=cb;
     document.head.appendChild(s);
   }
-  var done=0;
-  function tick(){done++;if(done>=3)go()}
+  var done=0,need=4;
+  function tick(){done++;if(done>=need)go()}
   loadScript("part1.js",tick);
   loadScript("part2.js",tick);
   loadScript("part3.js",tick);
-  setTimeout(function(){if(done<3)go()},3200);
+  loadScript("part4.js",tick);
+  setTimeout(function(){if(done<need)go()},4000);
 })();
